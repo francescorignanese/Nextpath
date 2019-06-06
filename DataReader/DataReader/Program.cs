543dac2064e26364f6d33e4c0fbdecf0378590a3
Alessandro Momesso <alessandro.momesso@tecnicosuperiorekennedy.it>
2019-06-06 15:47:13 +0200

    rimosso multithreading

--- DataReader/DataReader/Program.cs
+++ DataReader/DataReader/Program.cs
@@ -13,77 +13,44 @@ namespace DataReader
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Quanti autobus vuoi gestire? ");
-			int nBus = Convert.ToInt32(Console.ReadLine());
-			Thread[] buses = new Thread[nBus];
-			for (int i = 0; i < nBus; i++)
-			{
-				buses[i] = new Thread(Bus);
-				buses[i].Start();
-				Thread.Sleep(100);
+            string autobusTarga = System.Configuration.ConfigurationSettings.AppSettings["AutobusTarga"];
 
-			}
-		}
+            // init sensors
+            List<ISensor> sensors = new List<ISensor>
+                {
+                     new VirtualCoordinatesSensor(),
+                     new VirtualStopsSensor()
+                };
 
-		public static void Bus()
-		{
-			var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-			var stringChars = new char[5];
-			var random = new Random();
+            // configure Redis
+            string redisIp = System.Configuration.ConfigurationSettings.AppSettings["RedisIP"];
+            var redis = new RedisClient(redisIp);
 
-			for (int i = 0; i < stringChars.Length; i++)
-			{
-				stringChars[i] = chars[random.Next(chars.Length)];
-			}
+            string aux_type = "";
+            string aux_value = "";
 
-			var bus_id = new String(stringChars);
+            while (true)
+            {
+                foreach (ISensor sensor in sensors)
+                {                  
 
-			// init sensors
-			List<ISensor> sensors = new List<ISensor>
-				{
-					 new VirtualCoordinatesSensor(),
-					 new VirtualStopsSensor()
-				};
+                    aux_type = Convert.ToString(sensor.GetType());
 
-			// configure Redis
-			string redisIp = System.Configuration.ConfigurationSettings.AppSettings["RedisIP"];
-			var redis = new RedisClient(redisIp);
-
-			string aux_type = "";
-			string aux_value = "";
-
-			while (true)
-			{
-				foreach (ISensor sensor in sensors)
-				{
-					/*Console.WriteLine(sensor.GetType());
-					// get current sensor value
-					var data = sensor.ToJson();
-					Console.WriteLine(data);
-
-					// push to redis queue
-					redis.LPush("sensors_data", data);
-
-					// wait 1 second
-					System.Threading.Thread.Sleep(100);*/
-
-					//DataReader.Sensors.VirtualCoordinatesSensor
-					//DataReader.Sensors.VirtualStopsSensor
-
-					aux_type = Convert.ToString(sensor.GetType());
-					if (aux_type == "DataReader.Sensors.VirtualCoordinatesSensor")
-					{
-						aux_value = sensor.ToJson();
-					}
-					else
-					{
-						var data = "{\"bus_id\":\"" + bus_id + "\"," + aux_value + sensor.ToJson();
-						Console.WriteLine(data);
-						redis.LPush("sensors_data", data);
-						System.Threading.Thread.Sleep(10000);
-					}
-				}
-			}
-		}
+                    if (aux_type == "DataReader.Sensors.VirtualCoordinatesSensor")
+                    {
+                        aux_value = sensor.ToJson();
+                    }
+                    else
+                    {
+                        var data = "{\"bus_id\":\"" + autobusTarga + "\"," + aux_value + sensor.ToJson();
+                        Console.WriteLine(data);
+                        redis.LPush("sensors_data", data);
+                        System.Threading.Thread.Sleep(10000);
+                    }
+                }
+            }
+        }
+							
+		
 	}
 }
